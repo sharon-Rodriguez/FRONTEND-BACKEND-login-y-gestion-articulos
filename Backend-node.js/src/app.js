4fab41a9e14957c4516f64ecc75d702b8884ff59
Sharon Rodriguez <sharo2599@gmail.com>
2025-09-30 09:36:47 -0500

    backend: configurar servidor Express y conexión a MongoDB

--- Backend-node.js/src/app.js
+++ Backend-node.js/src/app.js
@@ -1,15 +1,23 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import articleRoutes from "./routes/article.routes.js";
+import cors from "cors";
 
 const app = express();
 
 //Middlewares
 app.use(express.json());
 
+// habilita las cors
+app.use(cors({
+    origin: "http://localhost:3000", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 //Rutas pricipales
 app.use("/api/auth", authRoutes); // monta las rutas de autenticacion
-app.use("/api/articles", articleRoutes);
+app.use("/api/articulos", articleRoutes);
 
 // Ruta de pruebas 
 app.get("/",(req, res) => {
--- Backend-node.js/src/config/db.js
+++ Backend-node.js/src/config/db.js
@@ -11,15 +11,3 @@ throw error; // lanzamos el error para manejarlos con server.js
 }
 }
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ Conectado a MongoDB");
-  } catch (error) {
-    console.error("❌ Error al conectar a MongoDB:", error.message);
-    process.exit(1);
-  }
-};
