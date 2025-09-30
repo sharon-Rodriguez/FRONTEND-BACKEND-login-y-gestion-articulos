bf4b2f514a850c6c074f0ebfcd39798e6b3163c0
Sharon Rodriguez <sharo2599@gmail.com>
2025-09-30 09:45:34 -0500

    backend: definir rutas para gestión de artículos

--- Backend-node.js/src/routes/article.routes.js
+++ Backend-node.js/src/routes/article.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createArticle, getArticles, deleteArticle } from "../controllers/article.controller.js";
+import { createArticle, getArticles, deleteArticle, updateArticle, getArticuloById } from "../controller/article.controller.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.get("/", getArticles);
+router.get("/:id", getArticuloById);
 router.post("/", authMiddleware, createArticle);
+router.put("/:id", authMiddleware, updateArticle);
 router.delete("/:id", authMiddleware, deleteArticle);
 
 export default router;
